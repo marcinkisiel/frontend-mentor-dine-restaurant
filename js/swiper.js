98cf69a55f7bd3c8518288107209b1f396c317ad
Marcin Kisiel <kisiel.marcin@gmail.com>
2020-11-11 21:29:11 +0100

    Add Swiper slider menu

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,21 @@
-var mySwiper = new Swiper('.swiper-container', {
+var eventSlider = new Swiper('.swiper-container', {
 	loop: false,
-
-	// navigation: {
-	// 	nextEl: '.swiper-button-next',
-	// 	prevEl: '.swiper-button-prev',
-	// },
 });
+
+const slide1 = document.querySelector('.event-slider__menu-item--1');
+const slide2 = document.querySelector('.event-slider__menu-item--2');
+const slide3 = document.querySelector('.event-slider__menu-item--3');
+
+slide1.addEventListener('click', function () {
+	goToSlide(0);
+});
+slide2.addEventListener('click', function () {
+	goToSlide(1);
+});
+slide3.addEventListener('click', function () {
+	goToSlide(2);
+});
+
+function goToSlide(number) {
+	eventSlider.slideTo(number);
+}
